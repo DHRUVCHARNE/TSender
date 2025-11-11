42b69fa7bf23f0bf07d442f1276eee9fc7677423
Dhruv Charne <94961914+DHRUVCHARNE@users.noreply.github.com>
2025-11-11 16:44:19 +0000

    Before deploy

--- test/playwright/basic.spec.ts
+++ test/playwright/basic.spec.ts
@@ -2,6 +2,7 @@
 import basicSetup from '../wallet-setup/basic.setup';
 import { testWithSynpress } from '@synthetixio/synpress';
 import { MetaMask,metaMaskFixtures } from '@synthetixio/synpress/playwright';
+import { anvil1Address, anvil2Address, mockTokenAddress, oneAmount } from '../test-constants'
 //data-testid="rk-wallet-option-metaMask"
 const test = testWithSynpress(metaMaskFixtures(basicSetup));
 const {expect} = test;
@@ -26,4 +27,21 @@ test("should show the airdrop form if connected, else not", async ({
   await page.getByTestId('rk-wallet-option-metaMask').waitFor({state:'visible',timeout:3000});
   await page.getByTestId('rk-wallet-option-metaMask').click();
   await metamask.connectToDapp();
+
+    const customNetwork = {
+    name: 'Anvil',
+    rpcUrl: 'http://127.0.0.1:8545',
+    chainId: 31337,
+    symbol: 'ETH'
+  }
+  await metamask.addNetwork(customNetwork)
+
+  await page.getByRole('textbox', { name: '0x', exact: true }).waitFor({
+    state: 'visible',
+    timeout: 30000
+  });
+  await page.getByRole('textbox', { name: '0x', exact: true }).fill(mockTokenAddress);
+  await page.getByRole('textbox', { name: '0x123..., 0x456...' }).fill(anvil2Address);
+  await page.getByRole('textbox', { name: '200, 300...' }).fill(oneAmount);
+  await expect(page.getByText('Token Name:Mock Token')).toBeVisible();
 })
